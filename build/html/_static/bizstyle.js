ba4350598d21b82b4230cca77bf252098350f1f2
Pashkov K.V <pk69@kruger.su>
2023-05-04 11:05:24 +0700

    ver 0.3

--- build/html/_static/bizstyle.js
+++ build/html/_static/bizstyle.js
@@ -23,7 +23,7 @@ const initialiseBizStyle = () => {
 }
 
 window.addEventListener("resize",
-  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "документация Интеграция Artix-УНФ 0.2")
+  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "документация Интеграция Artix-УНФ 0.3")
 )
 
 if (document.readyState !== "loading") initialiseBizStyle()
--- source/conf.py
+++ source/conf.py
@@ -9,7 +9,7 @@
 project = 'Интеграция Artix-УНФ'
 copyright = '2023, PashkovKV'
 author = 'PashkovKV'
-release = '0.2'
+release = '0.3'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
