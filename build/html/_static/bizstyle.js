ad0f087019c071baea77c55e340572453b53176a
Pashkov K.V <pk69@kruger.su>
2023-05-24 15:40:52 +0700

    0.91

--- build/html/_static/bizstyle.js
+++ build/html/_static/bizstyle.js
@@ -23,7 +23,7 @@ const initialiseBizStyle = () => {
 }
 
 window.addEventListener("resize",
-  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "документация Интеграция Artix-УНФ 0.91")
+  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "документация Интеграция Artix-УНФ 0.92")
 )
 
 if (document.readyState !== "loading") initialiseBizStyle()
